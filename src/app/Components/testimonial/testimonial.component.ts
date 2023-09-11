3e41046583d7a8ab894bca4b9f2c969db122fc05
elrodriguez <elrodriguez2423@gmail.com>
2023-09-11 12:36:21 -0500

    e

--- src/app/Components/testimonial/testimonial.component.ts
+++ src/app/Components/testimonial/testimonial.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-testimonial',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./testimonial.component.css']
 })
 export class TestimonialComponent {
-
+  @Input() data: any; 
 }
