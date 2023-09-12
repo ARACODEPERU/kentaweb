297a1af369fff6def2700780b75085a3cff2cd17
Kentha-SanIsidro <connexion.jesus@gmail.com>
2023-09-12 14:50:33 -0500

    avances en el home

--- src/app/Components/service/service.component.ts
+++ src/app/Components/service/service.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-service',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent {
-
+  @Input() data: any; 
 }
